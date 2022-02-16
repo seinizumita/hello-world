bab6553861879e40875164002ca8fa8d65bbb44e
Sein Izumita <sein.izumita@gmail.com>
2022-02-15 21:45:50 -0500

    clearing default homepage and adding courses component

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,9 +1,9 @@
 import { Template } from "@angular/compiler/src/render3/r3_ast";
 import { Component } from "@angular/core";
 
-@Component({
-    selector: 'courses',
-    template: '<h2>Courses</h2>'
+@Component({  //creating component decorator function
+    selector: 'courses', // <courses> = "courses", <div class="courses"> = .courses, <div id="courses"> = #courses
+    template: '<h2>Courses</h2>', //HTML markup to be rendered for this component
 
 })
 export class CoursescComponent{
