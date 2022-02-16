04a8a372b9fa9caf319014a49bb14b3a157fb7b7
Sein Izumita <sein.izumita@gmail.com>
2022-02-15 19:46:30 -0500

    asd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoursescComponent } from './courses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursescComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/courses.component.ts
@@ -0,0 +1,12 @@
+import { Template } from "@angular/compiler/src/render3/r3_ast";
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'courses',
+    template: '<h2>Courses</h2>'
+
+})
+export class CoursescComponent{
+
+
+}
