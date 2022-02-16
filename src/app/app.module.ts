0a9b6a656b6e6e12eaae61a0ba3ef52098778ade
Sein Izumita <sein.izumita@gmail.com>
2022-02-16 11:37:47 -0500

    adding courses component manually and using ng g c

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CoursescComponent } from './courses.component';
+import { CourseComponent } from './course/course.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CoursescComponent
+    CourseComponent
   ],
   imports: [
     BrowserModule,
